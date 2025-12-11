9fc8b3929825b01c7fb294eb11af81ac646dfe89
Gabriel PBB Farolito <148183275+ga0be@users.noreply.github.com>
2025-12-11 04:53:45 -0300

    Refactor comments and module name in extensao_sinal.v

--- extensao_sinal.v
+++ extensao_sinal.v
@@ -2,8 +2,9 @@
 //Projeto 2VA - Arquitetura e Organização de Computadores
 //2025.2
 
-module sign_extend(input wire[15:0] instruction, output wire[31:0] out); // entrada de 16 bits e saida de 32 bits
+module extensao_sinal(input wire[15:0] instruction, output wire[31:0] out); // entrada de 16 bits e saida de 32 bits
 	// repete 16 vezes o bit ins[15] (mais significativo) e faz concatenacao com a instrucao total
 	assign out = {{16{instruction[15]}}, instruction};
 	
+
 endmodule
